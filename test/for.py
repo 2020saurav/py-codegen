fc511387e63baa0ea4dc357109fa8ef4838afc57
Saurav Kumar <2020saurav@gmail.com>
2015-04-09 01:59:26 +0530

    Initial Commit : carrying forward from IR Gen Code

---
+++ test/for.py
@@ -0,0 +1,5 @@
+a = [1, 2, 3, 9, 7, 3]
+for i in a:
+	c = i * i + 3 / 2
+for var in [1,3,6,7]:
+	print var*var
---
+++ test/test2.py
@@ -0,0 +1,8 @@
+def f1(a):
+	return 2
+
+def f2(b):
+	print b+1
+a = [1,2,3,3]
+x = f1(2)
+print a[1]+x
